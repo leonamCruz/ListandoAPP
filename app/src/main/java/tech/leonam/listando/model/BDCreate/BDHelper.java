3a76af7a7c4bd1e0cc85e86210787c2109bd1a3e
Leonam <leonam447@gmail.com>
2023-03-14 17:15:44 -0300

    Create BDHelper

---
+++ app/src/main/java/tech/leonam/listando/model/BDCreate/BDHelper.java
@@ -0,0 +1,25 @@
+package tech.leonam.listando.model.BDCreate;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import tech.leonam.listando.R;
+
+public class BDHelper extends SQLiteOpenHelper {
+    private static final String NOME = String.valueOf(R.string.nomeBanco);
+    private static final int VERSAO = 1;
+    private static final String CRIAR_BD = "CREATE TABLE tarefas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT NOT NULL, descricao TEXT NOT NULL, prioridade INTEGER NOT NULL);";
+
+    public BDHelper(@Nullable Context context) {
+        super(context, NOME, null, VERSAO);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CRIAR_BD);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {}
+}
