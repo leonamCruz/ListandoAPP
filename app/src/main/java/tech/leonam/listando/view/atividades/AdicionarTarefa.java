83a55f8c25c828069781ba98c87a0b5ec71441e9
Leonam <leonam447@gmail.com>
2023-03-14 15:07:20 -0300

    att

---
+++ app/src/main/java/tech/leonam/listando/view/atividades/AdicionarTarefa.java
@@ -0,0 +1,16 @@
+package tech.leonam.listando.view.atividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tech.leonam.listando.R;
+
+public class AdicionarTarefa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa);
+    }
+}
--- app/src/main/java/tech/leonam/listando/view/fragmentos/ListaFragmento.java
+++ app/src/main/java/tech/leonam/listando/view/fragmentos/ListaFragmento.java
@@ -1,19 +1,37 @@
 package tech.leonam.listando.view.fragmentos;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import tech.leonam.listando.R;
+import tech.leonam.listando.view.atividades.AdicionarTarefa;
 
 public class ListaFragmento extends Fragment {
-
+    private FloatingActionButton floatingActionButton;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_lista, container, false);
+        var view = inflater.inflate(R.layout.fragment_lista, container, false);
+        iniciarComponentes(view);
+        click();
+        return view;
+    }
+
+    private void click() {
+        floatingActionButton.setOnClickListener(e->{
+            var intencao = new Intent();
+            intencao.setClass(e.getContext(), AdicionarTarefa.class);
+            startActivity(intencao);
+        });
+    }
+
+    private void iniciarComponentes(View view) {
+        floatingActionButton = view.findViewById(R.id.floatingActionButton);
     }
 }
